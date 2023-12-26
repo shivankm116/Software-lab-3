d9bfe5c7756e1e0d94a094cb2e40fb0419749c4e
shivankm116 <114577023+shivankm116@users.noreply.github.com>
2023-12-26 16:11:38 +0530

    Add files via upload

---
+++ WAP to find the circumference and area of a circle.py
@@ -0,0 +1,8 @@
+#6.WAP to find the circumference and area of a circle
+
+radius=5
+circumference=2*3.14*radius
+area=3.14*radius*radius
+print("RADIUS IS 5")
+print("AREA OF THE CIRCLE ::::",area)
+print("CIRCUMFERENCE OF THE CIRCLE ::::",circumference)
