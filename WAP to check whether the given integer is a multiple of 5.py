ce87ec443d3c8d63dbf0c9e7e3120d8348ef7037
shivankm116 <114577023+shivankm116@users.noreply.github.com>
2023-12-26 16:14:35 +0530

    Add files via upload

---
+++ WAP to check whether the given integer is a multiple of 5.py
@@ -0,0 +1,9 @@
+#7. WAP to check whether the given integer is a multiple of 5.
+i = int(input("enter the number"))
+
+if (i%5==0 ):
+
+         print (i, "is multiple of 5")
+else:
+
+         print (i, "is not a multiple of 5")
