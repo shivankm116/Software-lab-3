f60800a582f26bea39a3790b72589006965dcd82
shivankm116 <114577023+shivankm116@users.noreply.github.com>
2023-12-26 16:14:55 +0530

    Add files via upload

---
+++ WAP to check whether the given integer is a multiple of both 5 and 7.py
@@ -0,0 +1,9 @@
+#8. WAP to check whether the given integer is a multiple of both 5 and 7.
+i = int(input("enter the number"))
+
+if (i%5==0 and i%7==0 ):
+
+         print (i, "is multiple of both 5 and 7")
+else:
+
+         print (i, "is not a multiple of both 5 and 7")
