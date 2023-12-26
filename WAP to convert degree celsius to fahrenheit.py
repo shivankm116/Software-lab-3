417bb8880d027db2e9ff52a64fc0f3895743d300
shivankm116 <114577023+shivankm116@users.noreply.github.com>
2023-12-26 16:09:48 +0530

    Add files via upload

---
+++ WAP to convert degree celsius to fahrenheit.py
@@ -0,0 +1,4 @@
+# 2.WAP to convert degree celsius to fahrenheit.
+celsius = int(input("Enter the Temperature in Celsius :\n"))
+fahrenheit = (1.8 * celsius) + 32
+print("Temperature in Fahrenheit :", fahrenheit)
